3e92ba9830a80b3f3019d56e8dfb51ee3a66c17f
Pedro <palexb101@gmail.com>
2022-03-07 11:03:46 -0800

    assignment 4
    step 2,3, and 4

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,37 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/todos', function () {
+    return view('todos');
+});
+
+Route::get('/calendar', function () {
+    return view('calendar');
+});
+
+Route::get('/board', function () {
+    return view('board');
+});
+
+Route::get('events-feed/', function(){
+    return '[
+        {
+          "title": "CSE4500 Class",
+          "start": "2022-02-23T17:30:00",
+          "end": "2022-02-23T18:45:00"
+        },
+        {
+          "title": "CSE4500 Class",
+          "start": "2022-02-28T17:30:00",
+          "end": "2022-02-28T18:45:00"
+        }
+      ]';
+});
+
+Route::fallback(function () {
+    //
 });
