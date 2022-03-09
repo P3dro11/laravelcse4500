a2ac362ea5b371d69afe5599a4bb3b8e47830313
Pedro <palexb101@gmail.com>
2022-03-09 14:39:28 -0800

    Update web.php
    added  the  array for assignment 4

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/board', function () {
 });
 
 Route::get('events-feed/', function(){
-    return'[
+    $data = array '[
         {
           "title": "CSE4500 Class",
           "start": "2022-02-23T17:30:00",
@@ -40,7 +40,8 @@ Route::get('events-feed/', function(){
           "start": "2022-02-28T17:30:00",
           "end": "2022-02-28T18:45:00"
         }
-      ]';
+      ]'; 
+      return json.encode($data)
 });
 
 URL::forceScheme('https');
