124a254b3c849b2de7c5ee90c63d5b30eadb8034
Pedro <palexb101@gmail.com>
2022-03-09 21:27:21 -0800

    Update web.php
    editing 404 page

--- routes/web.php
+++ routes/web.php
@@ -29,18 +29,19 @@ Route::get('/board', function () {
 });
 
 Route::get('events-feed/', function(){
-    return'[
-        {
-          "title": "CSE4500 Class",
-          "start": "2022-02-23T17:30:00",
-          "end": "2022-02-23T18:45:00"
-        },
-        {
-          "title": "CSE4500 Class",
-          "start": "2022-02-28T17:30:00",
-          "end": "2022-02-28T18:45:00"
-        }
-      ]';
+    $data = array(
+        array(
+          'title' => 'CSE4500 Class',
+          'start' => "2022-02-23T17:30:00",
+          'end' => '2022-02-23T18:45:00'
+        ),
+        array (
+          'title' => 'CSE4500 Class',
+          'start' => '2022-02-28T17:30:00',
+          'end' => '2022-02-28T18:45:00'
+        )
+    );
+    return json_encode($data);
 });
 
 URL::forceScheme('https');
